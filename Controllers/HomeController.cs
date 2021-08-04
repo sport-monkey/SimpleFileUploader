6c7172d6efbaa0b60c47c1199959c77c96350e56
Ed Gough <ed.gough@gmail.com>
2021-08-04 20:41:08 +0100

    The code

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SimpleFileUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _destinationFolder;
+        public HomeController(IConfiguration configuration)
+        {
+            _destinationFolder = configuration["destinationFolder"];
+        }
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(false);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Index(List<IFormFile> files)
+        {
+            foreach (var formFile in files)
+            {
+                if (formFile.Length <= 0)
+                {
+                    continue;
+                }
+
+                var path = GenerateFilePath(formFile);
+
+                await using var file = System.IO.File.Create(path);
+                await formFile.CopyToAsync(file);
+            }
+            
+            return View(true);
+        }
+
+        private string GenerateFilePath(IFormFile formFile)
+        {
+            var fileName = formFile.FileName;
+            if (fileName.Length > 8)
+            {
+                fileName = fileName.Substring(fileName.Length - 8);
+            }
+
+            var path = Path.Combine(_destinationFolder, $"{Guid.NewGuid()}_{fileName}");
+            return path;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-SimpleFileUploader
+#SimpleFileUploader
+
+Requires [Net 5 SDK](https://dotnet.microsoft.com/download/dotnet/5.0)
+
+* Click the Code button above
+* Download Zip
+* Unpack to a folder
+* Type `cmd` in the navigation bar in explorer
+* Type `dotnet run`
+
+## Settings
+* Stored in appsettings.json
+* Alter `"DestinationFolder": "C:\\temp"`
+* Note that you need double `\\` for path separators
